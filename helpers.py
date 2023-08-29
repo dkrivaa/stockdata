5070a09751d95009372c3f2171ff04a7703b8de5
dkrivaa <dannykrivaa@hotmail.com>
2023-08-29 17:52:40 +0300

    stock data

--- helpers.py
+++ helpers.py
@@ -2,24 +2,30 @@ import requests
 import json
 import pandas as pd
 
-url = 'https://stockanalysis.com/api/screener/s/f?m=marketCap&s=desc&c=no,s,n,marketCap,price,change,revenue,volume,industry,sector,revenueGrowth,netIncome,fcf,netCash&cn=0&f=exchange-is-NASDAQ&p=2&dd=true&i=allstocks'
+def get_data():
+    url = 'https://stockanalysis.com/api/screener/s/f?m=marketCap&s=desc&c=no,s,n,marketCap,price,change,revenue,volume,industry,sector,revenueGrowth,netIncome,fcf,netCash&cn=0&f=exchange-is-NASDAQ&p=2&dd=true&i=allstocks'
+    response = requests.get(url)
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON data
+        stock_data = json.loads(response.text)
+        # Extract data
+        nested_data = stock_data['data']['data']
+        # Making dataframe with all stock data
+        df = pd.DataFrame(nested_data)
 
 
-
-
-
-
-url = 'https://stockanalysis.com/api/charts/s/aa/MAX/l/week'
-response = requests.get(url)
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Parse the JSON data
-    stock_data = json.loads(response.text)
-    nested_data = stock_data['data']
-    print(nested_data)
-    df = pd.DataFrame(nested_data)
-    print(df.shape)
-
+def stock_data(quote):
+    url = f'https://stockanalysis.com/api/charts/s/{quote}/MAX/l/week'
+    response = requests.get(url)
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON data
+        stock_data = json.loads(response.text)
+        # Extract data
+        nested_data = stock_data['data']['data']
+        # Making dataframe with all stock data
+        df = pd.DataFrame(nested_data)
 
 
 
