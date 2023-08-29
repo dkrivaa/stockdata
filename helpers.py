8614c2def300f1fa07188ceb89306293c063906b
dkrivaa <dannykrivaa@hotmail.com>
2023-08-29 18:15:23 +0300

    stock data

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import pandas as pd
+import datetime
 
 def get_data():
     url = 'https://stockanalysis.com/api/screener/s/f?m=marketCap&s=desc&c=no,s,n,marketCap,price,change,revenue,volume,industry,sector,revenueGrowth,netIncome,fcf,netCash&cn=0&f=exchange-is-NASDAQ&p=2&dd=true&i=allstocks'
@@ -19,7 +20,7 @@ def get_data():
         return df
 
 
-def stock_data(symbol):
+def specific_stock_data(symbol):
     url = f'https://stockanalysis.com/api/charts/s/{symbol}/MAX/l/week'
     response = requests.get(url)
     # Check if the request was successful (status code 200)
@@ -27,9 +28,14 @@ def stock_data(symbol):
         # Parse the JSON data
         stock_data = json.loads(response.text)
         # Extract data
-        nested_data = stock_data['data']['data']
+        nested_data = stock_data['data']
         # Making dataframe with all stock data
         df = pd.DataFrame(nested_data)
+        df = df.rename(columns={'t': 'date', 'c': 'close'})
+        df['date'] = df['date'].apply(lambda x: datetime.datetime.fromtimestamp(x))
+        df['date'] = df['date'].dt.date
+        df.drop('o', axis=1, inplace=True)
+        return df
 
 
 
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import pandas as pd
 import helpers
 
 df = helpers.get_data()
-print(df.shape)
-print(df.columns)
+print(df['symbol'][0])
+print(helpers.specific_stock_data(df['symbol'][0]))
 
 
 
